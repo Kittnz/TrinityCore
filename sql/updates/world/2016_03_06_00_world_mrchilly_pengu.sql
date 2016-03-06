6e901c53a0f2175ddcfcb0967c5a2e83eb60a9a2
Kittnz <frederik156@hotmail.com>
2016-03-06 17:13:05 +0100

    Pet/Misc: Implement Mr. Chilly & Pengu pet abilitys

---
+++ sql/updates/world/2016_03_06_00_world_mrchilly_pengu.sql
@@ -0,0 +1,2 @@
+UPDATE creature_template SET ScriptName = "npc_pet_gen_mr_chilly_pengu" WHERE entry = 29726;
+UPDATE creature_template SET ScriptName = "npc_pet_gen_mr_chilly_pengu" WHERE entry = 32595;
